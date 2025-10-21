38d09d0452c22dbef34608b6e59fd97d25e3dc6e
Hozana Silva Ferreira - FATEC - Cuiaba/MT <hozana.ferreira@alunosenai.mt>
2025-10-20 21:13:18 -0400

    Enviando o arquivo de login e readme

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,14 @@
+import  { View, StyleSheet } from 'react-native';
+
+export default function Login (){
+     return(
+        <View>
+
+        </View>
+     )
+
+}
+
+const style = StyleSheet.create({
+
+});
